b55da8cc9bd9ca896d4b74a3607d9f8fda10ed9e
SharkKnight4 <79238436+SharkKnight4@users.noreply.github.com>
2021-10-04 19:35:06 -0700

    Add files via upload
    Webcam.set and image classified and the snap function.

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-https://teachablemachine.withgoogle.com/models/knvQQDz_S/
+Webcam.set({
+    width:350,
+    height:300,
+    img_format:"jpeg",
+    jpeg_quality:100
+});
+camera=document.getElementById("camera");
+webcam.attach('#camera');
+function takesnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie" src="'+data_uri+'"/>'
+    });
+}
+console.log("ml5 version:",ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/knvQQDz_S/model.json',modelLoaded);
+function modelLoaded(){
+console.log('model is loaded');
+}
