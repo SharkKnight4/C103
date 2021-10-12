877860b60bd230c8b8b0124995f3e345cb02605c
SharkKnight4 <79238436+SharkKnight4@users.noreply.github.com>
2021-10-11 20:01:12 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ console.error(error);
     else{
         console.log(results);
         document.getElementById("result_name").innerHTML=results[0].label;
-        document.getElementById("result_accuracy").innerHTML=results[0].label.toFixed(3);
+        document.getElementById("result_accuracy").innerHTML=results[0].confidence.toFixed(3);
 
     }
 }
